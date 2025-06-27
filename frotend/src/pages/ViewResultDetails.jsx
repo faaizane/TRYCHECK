decfae83630414fa4e4ed81c8aa0b24dd0d9d0bf
Faizan <146511324+faaizane@users.noreply.github.com>
2025-06-28 02:35:41 +0500

    Update ViewResultDetails.jsx

--- frotend/src/pages/ViewResultDetails.jsx
+++ frotend/src/pages/ViewResultDetails.jsx
@@ -15,7 +15,7 @@ export default function ViewResultDetail() {
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    fetch(`${API_URL}/api/submissions/subject/${subjectId}`, {
+    fetch(`${import.meta.env.VITE_API_URL}/api/submissions/subject/${subjectId}`, {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then(res => res.json())
