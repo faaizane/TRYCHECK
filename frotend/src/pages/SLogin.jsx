13b7681fa96d1c7bf2d9dfb29209d5f0a2fd41fc
Faizan <146511324+faaizane@users.noreply.github.com>
2025-06-28 00:57:02 +0500

    Update SLogin.jsx

--- frotend/src/pages/SLogin.jsx
+++ frotend/src/pages/SLogin.jsx
@@ -19,7 +19,7 @@ export default function SLogin() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/auth/login', {
+      const res = await fetch('${API_URL}/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: regno, password })
